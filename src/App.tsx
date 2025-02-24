29ecb714831dd6118a41e0ec714933a7f67567d2
Branko <banne_nikolic@hotmail.com>
2025-02-24 20:10:22 +0100

    Refactor: Updated Product interface and mapped over product array.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+import { Product, ProductProps } from './components/Product'
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [productList, setProductList] = useState<ProductProps[]> ([
+    { 
+      name:"Spoon",
+      price: 50,
+      isOnSale: false,
+    },
+    {
+      name:"Fork",
+      price: 45,
+      isOnSale: true,
+    },
+    {
+      name: "Plate",
+      price: 100,
+      isOnSale: true
+    }
+  ]);
   return (
     <>
-      
+      <h1>Produkter</h1>
+      {productList.map((product, index) => (
+        <Product key={index} {...product} />
+      ))}
     </>
   )
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,5 +1,15 @@
-export interface Product{
-    name: string
-    price: number
-    isOnSale: boolean
-}
+export interface ProductProps {
+    name: string;
+    price: number;
+    isOnSale: boolean;
+  }
+  
+  export function Product({ name, price, isOnSale }: ProductProps) {
+    return (
+      <div>
+        <h2>{name}</h2>
+        <p>Pris: {price} kr</p>
+        {isOnSale && <p>På rea!</p>}
+      </div>
+    );
+  }
